895040a14bd30e9308fc029c57957fb6e1938787
Clement Escoffier <clement.escoffier@gmail.com>
2019-01-31 14:41:25 +0100

    Improvement around the AMQP receiver options.

--- src/main/java/io/vertx/ext/amqp/impl/AmqpConnectionImpl.java
+++ src/main/java/io/vertx/ext/amqp/impl/AmqpConnectionImpl.java
@@ -1,11 +1,11 @@
 package io.vertx.ext.amqp.impl;
 
-import io.vertx.core.*;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.ext.amqp.*;
-import io.vertx.proton.ProtonConnection;
-import io.vertx.proton.ProtonLinkOptions;
-import io.vertx.proton.ProtonReceiver;
-import io.vertx.proton.ProtonSender;
+import io.vertx.proton.*;
 
 import java.util.Objects;
 
@@ -35,17 +35,24 @@ public class AmqpConnectionImpl implements AmqpConnection {
   // TODO Allow creating a receiver just passing the handler
 
   @Override
-  public AmqpConnection receiver(String address, AmqpLinkOptions receiverOptions, Handler<AmqpMessage> handler,
+  public AmqpConnection receiver(String address, AmqpReceiverOptions receiverOptions, Handler<AmqpMessage> handler,
                                  Handler<AsyncResult<AmqpReceiver>> completionHandler) {
     Objects.requireNonNull(address, "The address must not be `null`");
     Objects.requireNonNull(handler, "The message handler must not be `null`");
     Objects.requireNonNull(completionHandler, "The completion handler must not be `null`");
-    ProtonLinkOptions options = new ProtonLinkOptions();
+    ProtonLinkOptions opts = new ProtonLinkOptions();
     if (receiverOptions != null) {
-      options = new ProtonLinkOptions().setDynamic(receiverOptions.isDynamicAddress())
-        .setLinkName(receiverOptions.getName());
+      opts = new ProtonLinkOptions()
+        .setDynamic(receiverOptions.isDynamic())
+        .setLinkName(receiverOptions.getLinkName());
     }
-    ProtonReceiver receiver = connection.createReceiver(address, options);
+    ProtonReceiver receiver = connection.createReceiver(address, opts)
+      .setAutoAccept(true);
+
+    if (receiverOptions != null) {
+      receiver.setQoS(ProtonQoS.valueOf(receiverOptions.getQos().toUpperCase()));
+    }
+
     receiver
       .handler((delivery, message) -> handler.handle(new AmqpMessageBuilder(message).build()))
       .openHandler(res -> {
@@ -56,7 +63,6 @@ public class AmqpConnectionImpl implements AmqpConnection {
         }
       })
       .open();
-
     return this;
   }
 
@@ -77,14 +83,14 @@ public class AmqpConnectionImpl implements AmqpConnection {
 
   @Override
   public AmqpConnection sender(String address, AmqpLinkOptions senderOptions, Handler<AsyncResult<AmqpSender>> completionHandler) {
-    ProtonLinkOptions options = new ProtonLinkOptions();
+    ProtonLinkOptions linkOptions = new ProtonLinkOptions();
     if (senderOptions != null) {
-      options = new ProtonLinkOptions()
+      linkOptions = new ProtonLinkOptions()
         .setDynamic(senderOptions.isDynamicAddress())
         .setLinkName(senderOptions.getName());
     }
 
-    ProtonSender sender = connection.createSender(address, options);
+    ProtonSender sender = connection.createSender(address, linkOptions);
     sender
       .openHandler(done -> {
         if (done.failed()) {
