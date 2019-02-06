d2b1b7e9dfa6e49e49667bd597ae91e7cd3e6fdc
Clement Escoffier <clement.escoffier@gmail.com>
2019-02-06 16:47:56 +0100

    Ensure the proton API is accessed on the right context

--- src/main/java/io/vertx/ext/amqp/impl/AmqpSenderImpl.java
+++ src/main/java/io/vertx/ext/amqp/impl/AmqpSenderImpl.java
@@ -1,6 +1,7 @@
 package io.vertx.ext.amqp.impl;
 
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.ext.amqp.AmqpMessage;
@@ -9,14 +10,18 @@ import io.vertx.proton.ProtonSender;
 
 public class AmqpSenderImpl implements AmqpSender {
   private final ProtonSender sender;
+  private final Context context;
 
-  public AmqpSenderImpl(ProtonSender sender) {
+  public AmqpSenderImpl(ProtonSender sender, Context context) {
     this.sender = sender;
+    this.context = context;
   }
 
   @Override
   public AmqpSender send(AmqpMessage message) {
-    sender.send(message.unwrap());
+    context.runOnContext(x -> {
+      sender.send(message.unwrap());
+    });
     return this;
   }
 
@@ -28,24 +33,27 @@ public class AmqpSenderImpl implements AmqpSender {
 
   @Override
   public AmqpSender sendWithAck(AmqpMessage message, Handler<AsyncResult<Void>> acknowledgementHandler) {
-    sender.send(message.unwrap(), delivery -> {
-      switch (delivery.getRemoteState().getType()) {
-        case Rejected:
-          acknowledgementHandler.handle(Future.failedFuture("message rejected (REJECTED"));
-          break;
-        case Modified:
-          acknowledgementHandler.handle(Future.failedFuture("message rejected (MODIFIED)"));
-          break;
-        case Released:
-          acknowledgementHandler.handle(Future.failedFuture("message rejected (RELEASED)"));
-          break;
-        case Accepted:
-          acknowledgementHandler.handle(Future.succeededFuture());
-          break;
-        default:
-          throw new UnsupportedOperationException("Unsupported delivery type: " + delivery.getRemoteState().getType());
-      }
+    context.runOnContext(x -> {
+      sender.send(message.unwrap(), delivery -> {
+        switch (delivery.getRemoteState().getType()) {
+          case Rejected:
+            acknowledgementHandler.handle(Future.failedFuture("message rejected (REJECTED"));
+            break;
+          case Modified:
+            acknowledgementHandler.handle(Future.failedFuture("message rejected (MODIFIED)"));
+            break;
+          case Released:
+            acknowledgementHandler.handle(Future.failedFuture("message rejected (RELEASED)"));
+            break;
+          case Accepted:
+            acknowledgementHandler.handle(Future.succeededFuture());
+            break;
+          default:
+            throw new UnsupportedOperationException("Unsupported delivery type: " + delivery.getRemoteState().getType());
+        }
+      });
     });
+
     return this;
   }
 
