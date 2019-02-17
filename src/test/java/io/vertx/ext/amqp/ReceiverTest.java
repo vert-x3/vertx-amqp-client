7d40e433460388674ab62d9ee017c1b2c47e935d
Clement Escoffier <clement.escoffier@gmail.com>
2019-02-17 18:49:01 +0100

    Test closing a consumer

--- src/test/java/io/vertx/ext/amqp/ReceiverTest.java
+++ src/test/java/io/vertx/ext/amqp/ReceiverTest.java
@@ -37,4 +37,6 @@ public class ReceiverTest extends ArtemisTestBase {
     await().until(() -> list.size() == 10);
     assertThat(list).containsExactly("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
   }
+
+
 }
