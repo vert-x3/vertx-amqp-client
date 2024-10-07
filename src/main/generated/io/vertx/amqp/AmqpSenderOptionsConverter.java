4d8de2a422d4c5ef80b16973b4f78ca02a5f0339
Julien Viet <julien@julienviet.com>
2024-10-07 23:42:28 +0200

    Regen converters

--- src/main/generated/io/vertx/amqp/AmqpSenderOptionsConverter.java
+++ src/main/generated/io/vertx/amqp/AmqpSenderOptionsConverter.java
@@ -4,16 +4,12 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
 
 /**
  * Converter and mapper for {@link io.vertx.amqp.AmqpSenderOptions}.
  * NOTE: This class has been automatically generated from the {@link io.vertx.amqp.AmqpSenderOptions} original class using Vert.x codegen.
  */
 public class AmqpSenderOptionsConverter {
-
-  private static final Base64.Decoder BASE64_DECODER = Base64.getUrlDecoder();
-  private static final Base64.Encoder BASE64_ENCODER = Base64.getUrlEncoder().withoutPadding();
 
    static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, AmqpSenderOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
